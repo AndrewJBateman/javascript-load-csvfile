281d86945aaf90ada2f8e7cde2c6413ab2aae8dd
Andrew Bateman <gomezbateman@yahoo.com>
2019-04-16 11:57:25 +0200

    readme replaced, comments added

--- README.md
+++ README.md
@@ -1,11 +1,77 @@
-# Javascript Load CSVFile
+# Javascript Load VSV File
 
 Code to learn to load a CSV file and manipulate its data. This is part of a Udemy Machine Learning course. The aim of the course is to understand the mathematics and programming techniques that are used in the most common Machine Learning algorithms.
 
-Uses the JavaScript utility library [Lodash](https://lodash.com/).
+**\* Note: to open web links in a new window use: _ctrl+click on link_**
 
-This project uses [node.js](https://nodejs.org) version 10.15.0 LTS.
+## Table of contents
 
-## Notes
+- [General info](#general-info)
+- [Screenshots](#screenshots)
+- [Technologies](#technologies)
+- [Setup](#setup)
+- [Features](#features)
+- [Status](#status)
+- [Inspiration](#inspiration)
+- [Contact](#contact)
 
-Link to Udemy Course [Machine Learning With Javascript](https://www.udemy.com/machine-learning-with-javascript/learn/v4/content).
+## General info
+
+- This is part of a course on Machine Learning and TensorFlow.
+
+## Screenshots
+
+![Example screenshot](./img/data-console-log.png).
+
+## Technologies
+
+- [node.js v10.15.0 LTS](https://nodejs.org).
+
+- [Lodash v4.17.11](https://lodash.com/) Javascript utility library.
+
+- [shuffle-seed v1.1.6](https://www.npmjs.com/package/shuffle-seed) a Nodejs module to Shuffle an Array of records by adding a seed phrase.
+
+## Setup
+
+Use 'node load-csv.js' in terminal to see console.logs or run functions.
+
+## Code Examples
+
+```javascript
+// main mapping statement, first row is skipped over,
+data = data.map((row, index) => {
+	if (index === 0) {
+		return row
+	}
+	//
+	return row.map((element, index) => {
+		// if there is a converters function then use function to return 'converted'
+		if (converters[headers[index]]) {
+			const converted = converters[headers[index]](element) // takes boolean TRUE and FALSE values
+			return _.isNaN(converted) ? element : converted // if not a number return element
+		}
+
+		// parseFloat returns the element and returns a floating point number.
+		const result = parseFloat(element) // changes strings to actual number values
+		return _.isNaN(result) ? element : result
+	})
+})
+```
+
+## Features
+
+- f
+
+## Status & To-Do List
+
+- Status: Working code.
+
+- To-Do: add to comments and try a more complex CSV data set.
+
+## Inspiration
+
+- [Udemy Course: Machine Learning With Javascript, Appendix Section 14](https://www.udemy.com/machine-learning-with-javascript/learn/v4/content)
+
+## Contact
+
+Created by [ABateman](https://www.andrewbateman.org) - feel free to contact me!
--- load-csv.js
+++ load-csv.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const _ = require('lodash');
 const shuffleSeed = require('shuffle-seed');
 
+// function to look at the data array and pick out the column names
 function extractColumns(data, columnNames) {
 	const headers = _.first(data);
 
@@ -13,10 +14,17 @@ function extractColumns(data, columnNames) {
 	return extracted;
 }
 
+/* function loadCSV takes 2 arguments: filename (a .csv file) and an array of options.
+It creates a variable data that uses the fs readFileSync function to return the contents of the path with utf-8 encoding.
+It splits the data on each new line to get an array of strings (one string per row).
+The map function then splits the rows on the ','. The data variable is overwritten each time.
+Then a lodash function dropRightWhile() is used to drop emty strings.
+A variable headers is created using the Lodash function that takes just the first row. 
+*/
 function loadCSV (
 	filename, 
 	{ 
-		converters = {}, 
+		converters = {}, // used to convert 'TRUE' to boolean true etc.
 		dataColumns = [], 
 		labelColumns = [],
 		shuffle = true,
@@ -28,19 +36,22 @@ function loadCSV (
 	data = data.map(row => _.dropRightWhile(row, val => val === ''));
 	const headers = _.first(data);
 
+	// main mapping statement, first row is skipped over, 
 	data = data.map((row, index) => {
 		if (index === 0) {
 			return row;
 		}
-
+		// 
 		return row.map((element, index) => {
+
+			// if there is a converters function then use function to return 'converted'
 			if (converters[headers[index]]) {
-				const converted = converters[headers[index]](element); //takes boolean TRUE and FALSE values
-				return _.isNaN(converted) ? element : converted; 
+				const converted = converters[headers[index]](element); // takes boolean TRUE and FALSE values
+				return _.isNaN(converted) ? element : converted; // if not a number return element
 			}
 
-
-			const result = parseFloat(element); //changes strings to actual number values
+			// parseFloat returns the element and returns a floating point number. 
+			const result = parseFloat(element); // changes strings to actual number values
 			return _.isNaN(result) ? element : result;
 		});
 	});
@@ -51,16 +62,20 @@ function loadCSV (
 	data.shift();
 	labels.shift();
 
+	// if shuffle option set to true then use shuffleseed to shuffle data
 	if (shuffle) {
 		data = shuffleSeed.shuffle(data, 'phrase');
 		labels = shuffleSeed.shuffle(labels, 'phrase');
 	}
 
+	// if this option is set to true (because a reduced training set is required for example) then create a training set
+	// based on the splitTest size, or just divide set in 2. 
 	if(splitTest) {
 		const trainSize = _.isNumber(splitTest) 
 		? splitTest 
 		: Math.floor(data.length / 2);
 
+		// use trainsize to slice data
 		return {
 			features: data.slice(trainSize),
 			labels: labels.slice(trainSize),
@@ -72,7 +87,6 @@ function loadCSV (
 		return { features: data, labels };
 	}
 
-	console.log(data);
 }
 
 const { features, labels, testFeatures, testLabels } = loadCSV('data.csv', {
@@ -91,6 +105,17 @@ console.log('Labels', labels);
 console.log('testFeatures', testFeatures);
 console.log('testLabels', testLabels);
 
-/*
-Lodash dropRightWhile creates a slice of array excluding elements dropped from the end. Elements are dropped until predicate returns falsey. The predicate is invoked with three arguments: (value, index, array).
+/*Notes
+Lodash dropRightWhile creates a slice of array excluding elements dropped from the end. 
+Elements are dropped until predicate returns falsey. 
+The predicate is invoked with three arguments: (value, index, array).
  */
+
+/*example options argument: property converters 
+	loadCSV('data.csv', {
+		converters: {
+			// passed: val => val === 'TRUE' // returns boolean true or false
+			passed: val => val === 'TRUE' ? 1 : 0
+		}
+	})
+*/
